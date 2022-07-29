662168dbf3e8b199a3d8e5e2d14a845f41d0835b
ricardober93 <ricardobzorro@gmail.com>
2022-07-29 10:36:17 -0500

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './style.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-mtmeo2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-mtmeo2)
