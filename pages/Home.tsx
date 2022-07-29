aa1c47956d88e8fe92cad2a13d61dd38a1cfc0e8
ricardober93 <ricardobzorro@gmail.com>
2022-07-29 12:04:19 -0500

    2 files modified

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import useMarvel from '../hooks/useMarvel';
 
 export default function Home() {
+  const { characters } = useMarvel();
   return (
     <div>
       <h1>hola home</h1>
